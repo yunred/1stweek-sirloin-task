bb5ead6a1fd441c9834ad9d9c39b44479df35203
WongueShin <won7963kr@gmail.com>
2022-01-27 20:02:47 +0900

    Modify Container/index.js props 인용방식을 수정했습니다! JSX를 그대로 사용하는건 오류가 나네요.

--- src/Component/Container/index.js
+++ src/Component/Container/index.js
@@ -3,8 +3,8 @@ const Container = (props) => {
     const ContainerContent = props.ContainerContent; 
     return(
         <>
-            <ContainerHeader/>
-            <ContainerContent/>
+            {ContainerHeader}
+            {ContainerContent}
         </>
         )
 }
