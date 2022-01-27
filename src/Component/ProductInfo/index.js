f604c35aeb45af2e3230f023440d88e69eb1cc3b
zerochae <zerochae@kakao.com>
2022-01-27 23:08:59 +0900

    Modifiy: 스타일 컴포넌트 분리,레이아웃 설계

--- src/Component/ProductInfo/index.js
+++ src/Component/ProductInfo/index.js
@@ -1,4 +1,5 @@
 import * as S from "./style";
+import SelectImg from 'Util/SelectImg';
 
 const ProductInfo = () => {
   return (
@@ -7,68 +8,100 @@ const ProductInfo = () => {
       <S.ItemContainer>
         <S.Item>
           <S.Title>카테고리</S.Title>
-          <S.InnerContainer>
-            <S.ListContainer>
-              <S.ListItem>카테고리명</S.ListItem>
-              <S.ListItem>카테고리명</S.ListItem>
-              <S.ListItem>카테고리명</S.ListItem>
-              <S.ListItem>카테고리명</S.ListItem>
-              <S.ListItem>카테고리명</S.ListItem>
-              </S.ListContainer>
-            <S.ListContainer>
-              <S.ListItem>선택한 카테고리 명 X </S.ListItem>
-              <S.ListItem>선택한 카테고리 명 X </S.ListItem>
-              <S.ListItem>선택한 카테고리 명 X </S.ListItem>
-              <S.ListItem>선택한 카테고리 명 X </S.ListItem>
-              <S.ListItem>선택한 카테고리 명 X </S.ListItem>
-              </S.ListContainer>
-          </S.InnerContainer>
-        </S.Item>
-        <S.Item>
-          <S.Title>필터 태그</S.Title>
-          <div>
-            <div>
-              <input /> <button>검색</button>{" "}
-            </div>
-          </div>
-        </S.Item>
-        <S.Item>
-          <S.Title>상품명</S.Title>
-          <S.InnerContainer>
-            <input type="text" />{" "}
-            <div>
-              {" "}
-              <span>상품 코드</span> <span>상품코드</span>{" "}
-            </div>{" "}
-          </S.InnerContainer>
-        </S.Item>
-        <S.Item>
-          <S.Title>상품 구성 소개 정보</S.Title>
-          <div>
-            <input type="text" />
-          </div>
-        </S.Item>
-        <S.Item>
-          <S.Title>상품 썸네일</S.Title>
-          <S.InnerContainer>
-            <div>+이미지 첨부</div> 
-            <S.ListContainer>
-            <S.ListItem>이미지 파일명.jpg</S.ListItem>
-            <S.ListItem>이미지 파일명.jpg</S.ListItem>
-            <S.ListItem>이미지 파일명.jpg</S.ListItem>
-            <S.ListItem>이미지 파일명.jpg</S.ListItem>
-            <S.ListItem>이미지 파일명.jpg</S.ListItem>
+          <S.InnerContainer width={`500px`}>
+            <S.ListContainer width={`50%`}>
+              <S.ListItem>
+                <S.Check type="checkbox" />
+                카테고리명
+              </S.ListItem>
+              <S.ListItem>
+                <S.Check type="checkbox" />
+                카테고리명
+              </S.ListItem>
+              <S.ListItem>
+                <S.Check type="checkbox" />
+                카테고리명
+              </S.ListItem>
+              <S.ListItem>
+                <S.Check type="checkbox" />
+                카테고리명
+              </S.ListItem>
+              <S.ListItem>
+                <S.Check type="checkbox" />
+                카테고리명
+              </S.ListItem>
+              <S.ListItem>
+                <S.Check type="checkbox" />
+                카테고리명
+              </S.ListItem>
+              <S.ListItem>
+                <S.Check type="checkbox" />
+                카테고리명
+              </S.ListItem>
+            </S.ListContainer>
+            <S.ListContainer width={`30%`}>
+              <S.Tag>선택한 카테고리 명 X </S.Tag>
+              <S.Tag>선택한 카테고리 명 X </S.Tag>
+              <S.Tag>선택한 카테고리 명 X </S.Tag>
+              <S.Tag>선택한 카테고리 명 X </S.Tag>
+              <S.Tag>선택한 카테고리 명 X </S.Tag>
             </S.ListContainer>
           </S.InnerContainer>
         </S.Item>
         <S.Item>
+          <S.Title>필터 태그</S.Title>
+          <S.InnerContainer>
+            <S.InputContainer>
+              <input type="text" placeholder="필터태그를 검색해 주세요." />{" "}
+              <button>검색</button>
+            </S.InputContainer>
+          </S.InnerContainer>
+        </S.Item>
+        <S.Item>
+          <S.Title>상품명</S.Title>
+          <S.InnerContainer>
+            <S.InputContainer>
+              <input type="text" />
+            </S.InputContainer>
+            <div>
+              <span>상품 코드</span> <span>상품코드</span>
+            </div>
+          </S.InnerContainer>
+        </S.Item>
+        <S.Item>
+          <S.Title>상품 구성 소개 정보</S.Title>
+          <S.InnerContainer>
+            <S.InputContainer>
+              <input type="text" />
+            </S.InputContainer>
+          </S.InnerContainer>
+        </S.Item>
+        <S.Item>
+          <S.Title>상품 썸네일</S.Title>
+          <S.InnerContainer>
+            <SelectImg/>
+            <div>이미지 파일명.jpg</div>
+            
+          </S.InnerContainer>
+        </S.Item>
+        <S.Item>
           <S.Title>상품 대표 이미지</S.Title>
-          <div>+이미지 첨부</div>
-          <div>이미지 파일명.jpg</div>
+          <S.InnerContainer>
+            <SelectImg/>
+            <S.ListContainer>
+              <S.ListItem>이미지 파일명.jpg</S.ListItem>
+              <S.ListItem>이미지 파일명.jpg</S.ListItem>
+              <S.ListItem>이미지 파일명.jpg</S.ListItem>
+              <S.ListItem>이미지 파일명.jpg</S.ListItem>
+              <S.ListItem>이미지 파일명.jpg</S.ListItem>
+            </S.ListContainer>
+          </S.InnerContainer>
         </S.Item>
         <S.Item>
           <S.Title>상품 총 재고</S.Title>
-          <span>NN개</span>
+          <S.InnerContainer>
+            <span>NN개</span>
+          </S.InnerContainer>
         </S.Item>
       </S.ItemContainer>
     </S.CategoryContainer>
