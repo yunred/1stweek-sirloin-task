f2db1c97284f8a14ca3e6558157c4d9917ab4ac4
WongueShin <won7963kr@gmail.com>
2022-01-27 23:34:07 +0900

    Modify SelectImg, Add MDOption SelectImg에 useCallbakc을 적용, MDOption 컴포넌츠 생성, 초기상태와 받게될 props를 주석으로 정의했습니다.

---
+++ src/Component/MDOption/index.js
@@ -0,0 +1,52 @@
+import react from "react";
+import Container from "Component/Container";
+import * as S from "./style"
+
+/*
+props = {
+    optionSetList 옵션세트정보를 원소로 하는 배열 ( state ) = [  
+        { dict 하나당 독립적인 option set의 정보를 담는다.
+            optionImg : []; len === 0일경우 selecImg를, len > 0 일경우 이미지를 표시. 
+            optionList : [
+                {
+                    optionName: ''
+                    optionValue: [상품 정상가'int', 상품 판매가'int', 재고'int', 과세여부'bool']
+                    addtional: [[추가 옵션명'str', 추가옵션 정상가'int']]
+                }
+            ]
+        }
+    ]
+}
+*/
+
+export const MDOheader = (props) => {
+    const optionSetter = props.optionSetter;
+
+    return(
+        <>
+            <S.ContainerName>상품 옵션</S.ContainerName>
+            <S.OptionSetAppederButton>+ 옵션 세트 추가</S.OptionSetAppederButton>
+        </>
+    )
+}
+
+export const MDOContent = (props) => {
+
+    return(
+        <S.DefaultScreen>
+            <S.H3>옵션 세트를 추가하여 옵션을 구성해 주세요</S.H3>
+        </S.DefaultScreen>
+    )
+}
+
+const MDOption = (props) => {
+    const optionState = props.optionState;
+    const optionSetter = props.optionSetter;
+    return(
+        <Container ContainerHeader = {<MDOheader/>} ContainerContent = {<MDOContent/>}/>
+    )
+}
+
+
+
+export default MDOption
