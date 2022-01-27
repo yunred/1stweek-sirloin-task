d6cb411a8d76116214981ce8c0e39c661365217c
jyb8509 <bestdung8509@gmail.com>
2022-01-27 20:23:10 +0900

    Modify: GlobalStyle 경로 수정

--- src/Component/Container/index.js
+++ src/Component/Container/index.js
@@ -1,12 +1,12 @@
 const Container = (props) => {
-    const ContainerHeader = props.ContainerHeader;
-    const ContainerContent = props.ContainerContent; 
-    return(
-        <>
-            {ContainerHeader}
-            {ContainerContent}
-        </>
-        )
-}
+  const ContainerHeader = props.ContainerHeader;
+  const ContainerContent = props.ContainerContent;
+  return (
+    <>
+      {ContainerHeader}
+      {ContainerContent}
+    </>
+  );
+};
 
-export default Container
+export default Container;
