cd5ed05bf7a72b61232b08d69d044b92313d9073
jyb8509 <bestdung8509@gmail.com>
2022-01-27 21:59:14 +0900

    Add : Nav 레이아웃 추가

--- src/Component/Nav/Nav.js
+++ src/Component/Nav/Nav.js
@@ -4,58 +4,32 @@ import styled from "styled-components";
 const NavContainer = styled.ul`
   list-style-type: none;
   width: 25%;
-  position: fixed;
   height: 100%;
-  overflow: auto;
 `;
 
 const NavMenu = styled.li``;
 
-
-
-
-
-
 const Nav = () => {
   const menu = [
-    {id: '1',
-      title: '기본설정'
-    },
-    {id: '2',
-    title: '회원'
-    },
-    {id: '3',
-    title: '진열'
-    },
-    {id: '4',
-    title: '상품'
-    },
-    {id: '5',
-    title: '주문'
-    },
-    {id: '6',
-    title: '배송'
-    },
-    {id: '7',
-    title: '기본설정'
-    },
-  ]
+    { id: "1", title: "기본설정" },
+    { id: "2", title: "회원" },
+    { id: "3", title: "진열" },
+    { id: "4", title: "상품" },
+    { id: "5", title: "주문" },
+    { id: "6", title: "배송" },
+    { id: "7", title: "프로모션" },
+    { id: "8", title: "혜택" },
+    { id: "9", title: "혜택" },
+    { id: "10", title: "고객 센터 관리" },
+    { id: "11", title: "알림" },
+  ];
 
   return (
-    <>
-      <NavContainer>
-        <NavMenu props={기본설정}/>
-        <NavMenu props={회원}/>
-        <NavMenu props={진열}/>
-        <NavMenu props={상품}/>
-        <NavMenu props={주문}/>
-        <NavMenu props={배송}/>
-        <NavMenu props={프로모션}/>
-        <NavMenu props={혜택}/>
-        <NavMenu props={고객 센터 관리}/>
-        <NavMenu props={알림}/>
-      </NavContainer>
-    </>
+    <NavContainer>
+      {menu.map((index, title) => (
+        <NavMenu key={index}>{index.title}</NavMenu>
+      ))}
+    </NavContainer>
   );
 };
 
