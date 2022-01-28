574cc8bd2ca9af0b10d5051a615e176733563d13
zerochae <zerochae@kakao.com>
2022-01-28 12:12:03 +0900

    Add: ProductInfo 전역 state 등록

--- src/App.js
+++ src/App.js
@@ -2,21 +2,22 @@ import { useState } from "react";
 import GlobalStyle from "Style/GlobalStyle";
 import Nav from "Component/Nav/Nav";
 import MDOption from "Component/MDOption";
-import MDdata from "store/MDdata.js"
+import MDdata from "store/MDdata.js";
 import ProductInfo from "Component/ProductInfo";
+import PIdata from "store/PIData";
 
-  
+function App() {
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
       <MDdata>
-        <MDOption/>
+        <MDOption />
       </MDdata>
-      <ProductInfo productInfoList={productInfoList} PISetter={PISetter}/>
-
+      <PIdata>
+        <ProductInfo />
+      </PIdata>
     </div>
   );
 }
-
 export default App;
