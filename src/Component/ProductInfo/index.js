83614a39a81e58b80dc91ca37fe3c0c3f3f41b0d
zerochae <zerochae@kakao.com>
2022-01-28 15:16:56 +0900

    Modify: store/PDdata.PIData 초기 데이터 설정

--- src/Component/ProductInfo/index.js
+++ src/Component/ProductInfo/index.js
@@ -1,26 +1,36 @@
 import * as S from "./style";
 import Container from "Component/Container";
 import SelectImg from "Util/SelectImg";
+import React,{ useContext,useEffect } from "react";
+import { PDcontext } from "store/PDdata.js";
 
 /*
 
 간략히=======================
 
+이미지 
+
+state X -> useEffect
+
 props = {
 
   ProductInfoList = {
-    category:[],
-    filterTag:[],
+
+    categoryList:[],
+    filterTagList:[],
+
     Product:{
       category:[],
       filterTag:[],
+      name:"",
       description:"",
       thumbnail:"",
       imgs:"",
       stock:"",
     }
+
   }
-  
+
 }
 
 
@@ -80,20 +90,6 @@ props = {
 }
 */
 
-export const PIHeader = (props) => {
-  return <h4>상품 기본 정보</h4>;
-};
-
-export const PIContent = (props) => {
-  return (
-    <S.ItemContainer>
-      {item.map((item) => {
-        return <S.Item key={item.idx}>{item.content}</S.Item>;
-      })}
-    </S.ItemContainer>
-  );
-};
-
 const ProductInfo = () => {
   return (
     <S.ProductInfoContainer>
@@ -104,6 +100,142 @@ const ProductInfo = () => {
     </S.ProductInfoContainer>
   );
 };
+
+export const PIHeader = (props) => {
+  return <h4>상품 기본 정보</h4>;
+};
+
+export const PIContent = (props) => {
+  const context = useContext(PDcontext).PIData;
+  const state = context.state;
+  const setState = context.setState;
+
+  useEffect(() => {
+    console.log("ProductInfo: ",state)
+  }, [state]);
+  
+  const handleProductDescription = (e) => {
+
+    const newState = {...state }
+
+    newState.product.description = e.target.value;
+
+    setState(newState)
+  }
+
+  const handleProductName = (e) => {
+    const newState = {...state }
+
+    newState.product.name = e.target.value;
+
+    setState(newState)
+  }
+
+  const handleProductThumbnail = () => {
+    const newState = {...state}
+  }
+
+  const handleProductImg = () => {
+    const newState = { ...state }
+  }
+
+
+  return (
+    <S.ItemContainer>
+      <S.Item>
+        <S.Title>카테고리</S.Title>
+        <S.InnerContainer width={`500px`}>
+          <S.ListContainer width={`50%`}>
+            {category.map((item) => {
+              return (
+                <S.ListItem key={item}>
+                  <S.Check type="checkbox" />
+                  {item}
+                </S.ListItem>
+              );
+            })}
+          </S.ListContainer>
+          <S.ListContainer width={`30%`}>
+            {selectedCategory.map((item) => {
+              return (
+                <S.Tag key={item}>
+                  {item}
+                  <button>X</button>
+                </S.Tag>
+              );
+            })}
+          </S.ListContainer>
+        </S.InnerContainer>
+      </S.Item>
+      <S.Item>
+        <S.Title>필터 태그</S.Title>
+        <S.InnerContainer>
+          <S.InputContainer>
+            <input type="text" placeholder="필터태그를 검색해 주세요." />
+            <button>검색</button>
+          </S.InputContainer>
+        </S.InnerContainer>
+      </S.Item>
+      <S.Item>
+        <S.Title>상품명</S.Title>
+        <S.InnerContainer>
+          <S.InputContainer>
+            <input
+              type="text"
+              placeholder="상품명을 입력해주세요."
+              onChange={(e) => {
+                handleProductName(e);
+              }}
+            />
+          </S.InputContainer>
+          <S.ProductContainer>
+            <span>상품 코드</span> <span>상품코드</span>
+          </S.ProductContainer>
+        </S.InnerContainer>
+      </S.Item>
+      <S.Item>
+        <S.Title>상품 구성 소개 정보</S.Title>
+        <S.InnerContainer>
+          <S.InputContainer>
+            <input
+              type="text"
+              onChange={(e)=>{handleProductDescription(e)}}
+              placeholder="상품 구성 소개 정보를 입력해 주세요."
+            />
+          </S.InputContainer>
+        </S.InnerContainer>
+      </S.Item>
+      <S.Item>
+        <S.Title>상품 썸네일</S.Title>
+        <S.InnerContainer>
+          <SelectImg className="productInfo-img-btn" />
+          <div>이미지 파일명.jpg</div>
+        </S.InnerContainer>
+      </S.Item>
+      <S.Item>
+        <S.Title>
+          상품 대표 <br />
+          이미지
+        </S.Title>
+        <S.InnerContainer>
+          <SelectImg className="productInfo-img-btn" />
+          <S.ListContainer>
+            {imgs.map((item) => {
+              return <S.ListItem key={item}>{item}</S.ListItem>;
+            })}
+          </S.ListContainer>
+        </S.InnerContainer>
+      </S.Item>
+      <S.Item>
+        <S.Title>상품 총 재고</S.Title>
+        <S.InnerContainer>
+          <span>NN개</span>
+        </S.InnerContainer>
+      </S.Item>
+    </S.ItemContainer>
+  );
+};
+
 
 const category = [
   "카테고리1",
@@ -130,126 +262,4 @@ const imgs = [
   "이미지5.jpg",
 ];
 
-const item = [
-  {
-    idx: "1",
-    content: (
-      <>
-        <S.Title>카테고리</S.Title>
-        <S.InnerContainer width={`500px`}>
-          <S.ListContainer width={`50%`}>
-            {category.map((item) => {
-              return (
-                <S.ListItem key={item}>
-                  <S.Check type="checkbox" />
-                  {item}
-                </S.ListItem>
-              );
-            })}
-          </S.ListContainer>
-          <S.ListContainer width={`30%`}>
-            {selectedCategory.map((item) => {
-              return (
-                <S.Tag key={item}>
-                  {item}
-                  <button>X</button>
-                </S.Tag>
-              );
-            })}
-          </S.ListContainer>
-        </S.InnerContainer>
-      </>
-    ),
-  },
-  {
-    idx: "2",
-    content: (
-      <>
-        <S.Title>필터 태그</S.Title>
-        <S.InnerContainer>
-          <S.InputContainer>
-            <input type="text" placeholder="필터태그를 검색해 주세요." />
-            <button>검색</button>
-          </S.InputContainer>
-        </S.InnerContainer>
-      </>
-    ),
-  },
-  {
-    idx: "3",
-    content: (
-      <>
-        <S.Title>상품명</S.Title>
-        <S.InnerContainer>
-          <S.InputContainer>
-            <input type="text" placeholder="상품명을 입력해주세요." />
-          </S.InputContainer>
-          <S.ProductContainer>
-            <span>상품 코드</span> <span>상품코드</span>
-          </S.ProductContainer>
-        </S.InnerContainer>
-      </>
-    ),
-  },
-  {
-    idx: "4",
-    content: (
-      <>
-        {" "}
-        <S.Title>상품 구성 소개 정보</S.Title>
-        <S.InnerContainer>
-          <S.InputContainer>
-            <input
-              type="text"
-              placeholder="상품 구성 소개 정보를 입력해 주세요."
-            />
-          </S.InputContainer>
-        </S.InnerContainer>
-      </>
-    ),
-  },
-  {
-    idx: "5",
-    content: (
-      <>
-        <S.Title>상품 썸네일</S.Title>
-        <S.InnerContainer>
-          <SelectImg className="productInfo-img-btn" />
-          <div>이미지 파일명.jpg</div>
-        </S.InnerContainer>
-      </>
-    ),
-  },
-  {
-    idx: "6",
-    content: (
-      <>
-        <S.Title>
-          상품 대표 <br />
-          이미지
-        </S.Title>
-        <S.InnerContainer>
-          <SelectImg className="productInfo-img-btn" />
-          <S.ListContainer>
-            {imgs.map((item) => {
-              return <S.ListItem key={item}>{item}</S.ListItem>;
-            })}
-          </S.ListContainer>
-        </S.InnerContainer>
-      </>
-    ),
-  },
-  {
-    idx: "7",
-    content: (
-      <>
-        <S.Title>상품 총 재고</S.Title>
-        <S.InnerContainer>
-          <span>NN개</span>
-        </S.InnerContainer>
-      </>
-    ),
-  },
-];
-
-export default ProductInfo;
+export default React.memo(ProductInfo);
