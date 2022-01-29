d2658cd2bd6b1a65824b77dc21d16114ac67e007
SEOYUN KIM <nado0903@gmail.com>
2022-01-29 09:28:13 +0900

    Fix: conflict 해결

---
+++ src/Component/ProductDelivery/data.js
@@ -0,0 +1,10 @@
+const PDInfo = {
+  isDesignated: false,
+  isPickup: false,
+  ispreOrder: false,
+  orderPeriod: [],
+  earlyMorningDate: '',
+  normalDate: '',
+};
+
+export default PDInfo;
---
+++ src/Component/ProductInfoNotice/data.js
@@ -0,0 +1,10 @@
+const PIOrder = {
+  productname: '',
+  origin: '',
+  rating: '',
+  productstorage: '',
+  category: '',
+  moreinfo: [],
+};
+
+export default PIOrder;
